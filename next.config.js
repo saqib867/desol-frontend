80f80a132541e0b9d54fb571a4a7256ec73ddfaf
saqib867 <saqibhussain9910@gmail.com>
2023-11-25 14:44:23 +0500

    changes made

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
-const API_URL = "https://desol-backend-kappa.vercel.app"
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	async rewrites() {
-		return [
-			{
-				source: '/api/:path*',
-				destination: `${API_URL}/:path*`,
-			},
-		]
-	},
-}
+const nextConfig = {}
 
 module.exports = nextConfig
