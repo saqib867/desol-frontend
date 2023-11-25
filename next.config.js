9e5174abf0d06d360b2e67ee9453b50791f795c8
saqib867 <saqibhussain9910@gmail.com>
2023-11-25 14:39:38 +0500

    changes made

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
-module.exports = {
-    async headers() {
-      return [
-        {
-          // matching all API routes
-          source: "/api/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-        }
-      ]
-    },
-  };
+const API_URL = "https://desol-backend-kappa.vercel.app"
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: `${API_URL}/:path*`,
+			},
+		]
+	},
+}
+
+module.exports = nextConfig
